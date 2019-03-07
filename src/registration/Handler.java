1366f041947fb30b605a3c82ff5d2bae0fe1d9f1
Asgar1121 <asgar11212@gmail.com>
2019-03-07 19:58:33 +0000

    made some verification checks, need to add in hash code and database functionality

--- src/registration/Handler.java
+++ src/registration/Handler.java
@@ -3,6 +3,8 @@ package registration;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Handler implements Runnable {
     private Socket socket;
@@ -56,28 +58,49 @@ public class Handler implements Runnable {
     }
 
     public void registrationCode() {
-        boolean passwordPhase = false; // boolean to tell us whether we are reading in the password or username
+        boolean usernamePhase = false; // boolean to tell us whether we are reading in the email or username
+        boolean passwordPhase = false; // boolean to tell us whether we are reading in the username or password
         int passwordInputCount = 0; // counter to see which input of the password we are on, should be max of 1
+
+        String email = "";
+        String username = "";
+        String hashedPassword = "";
 
         System.out.println("Connected to socket: " + socket); // connection message
 
         StringBuffer sb = new StringBuffer();
-        out.println("Please enter a username of one word length!");
+        out.println("Please enter an email address!");
         while(in.hasNextLine()) { // while a line is coming in from the socket (keeps reading in)
             sb.append(in.nextLine()); // adds the read in word to the StringBuffer
-            if(!passwordPhase) { // in username phase
+            if(!usernamePhase) { // reading in email
+                if(!isValidEmailFormat(sb.toString())) {
+                    out.println("The email address provided is not valid! Please enter a new email address.");
+                }
+                else {
+                    email = sb.toString(); // update email field
+                    setEmailInDatabase(email);
+                    out.println("The email provided is of valid format! Now please enter a username that is one word in length and contains only numbers and letters.");
+                    sb.setLength(0);
+                    usernamePhase = true;
+                }
+            }
+            else if (!passwordPhase) { // in username phase
                 if (!isValidUsername(sb.toString())) {
                     out.println("The username provided is not valid or is already in use! Please enter a new username" +
-                            " that is only one word long.");
-                } else {
+                            " that is only one word long and contains only numbers and letters.");
+                }
+                else {
+                    username = sb.toString(); // update username variable
+                    setUsernameInDatabase(username);
                     out.println("Congratulations! Your username is now: " + sb.toString() + ". Now please enter a password!"); // print out that line to the client
                     sb.setLength(0); // clears the word in the StringBuffer
                     passwordPhase = true; // set passwordPhase to true since we will be entering it next
                 }
             }
             else { // in password phase
-                if(passwordInputCount == 1) { // already entered password before so this time we want to verify it
-                    if(confirmPassword(sb.toString())) {
+                if (passwordInputCount == 1) { // already entered password before so this time we want to verify it
+                    if (confirmHashOfPassword(sb.toString())) {
+                        setPasswordInDatabase(hashedPassword);
                         out.println("You have successfully verified your password, your account has now been created!");
                         break;
                     }
@@ -87,23 +110,46 @@ public class Handler implements Runnable {
                         passwordInputCount = 0;
                     }
                 }
-                setPasswordInDatabase(sb.toString()); // put password in database
+                hashedPassword = computeHashOfPassword(sb.toString()); // update password field
                 passwordInputCount++; // update the password counter to 1 so we can then verify it
                 out.println("Now please verify your password!");
             }
         }
     }
 
+    public boolean isValidEmailFormat(String email) {
+        if(email.equals("")) { return false; }
+        else {
+			// regex taken from website http://emailregex.com/
+            String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+            Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(email);
+            return matcher.matches();
+        }
+    }
+
     public boolean isValidUsername(String username) {
-        // check if not existing in database already and that it is only one word
-        return true;
+        // check if not existing in database already and that it is only one word and that it is not blank
+        return username.matches("^[a-zA-Z0-9]+");
+    }
+
+    public void setEmailInDatabase(String email) {
+        // set username in database
+    }
+
+    public void setUsernameInDatabase(String username) {
+        // set username in database
     }
 
     public void setPasswordInDatabase(String password) {
         // set hashed password in database
     }
 
-    public boolean confirmPassword(String passwordToVerify) {
+    public String computeHashOfPassword(String password) {
+        return password;
+    }
+
+    public boolean confirmHashOfPassword(String passwordToVerify) {
         // check if the second password input matches the first given one
         return true;
     }
