8f26f1304367208acd475f9992be0a5e51aa6b5e
Asgar1121 <asgar11212@gmail.com>
2019-03-08 13:32:37 +0000

    new registration code using object input/output with custom message class to distinguish mesage type, still need to correct database code and should be good to go for registration only

---
+++ src/registration2/Handler2.java
@@ -0,0 +1,146 @@
+package registration2;
+
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Handler2 implements Runnable {
+    private Socket socket;
+    private InputStream is;
+    private OutputStream os;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+
+    public Handler2(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            os = socket.getOutputStream();
+            out = new ObjectOutputStream(os); // create a writer to write to the socket
+            is = socket.getInputStream();
+            in = new ObjectInputStream(is); // create a scanner to read in from the socket
+
+            Message m = (Message)in.readObject();
+            if (m.getType().equals(Server2.QueryType.REGISTRATION)) { // checks that the action is a registration action
+                registrationCode(); // run the registration code since the action is to do with registration
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                socket.close();
+                if (socket.isClosed()) {
+                    System.out.println("The socket " + socket + " is now closed!");
+                }
+            } catch (Exception e) {
+                System.out.println("Error in socket: " + socket);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void registrationCode() throws Exception {
+        boolean usernamePhase = false; // boolean to tell us whether we are reading in the email or username
+        boolean passwordPhase = false; // boolean to tell us whether we are reading in the username or password
+        int passwordInputCount = 0; // counter to see which input of the password we are on, should be max of 1
+
+        String email = " ";
+        String username = "";
+        String hashedPassword = "";
+
+        System.out.println("Connected to socket: " + socket); // connection message
+
+        StringBuffer sb = new StringBuffer();
+        Message m;
+        out.writeObject(new Message(Server2.QueryType.REGISTRATION, "Please enter an email address!"));
+        while (true) { // while a line is coming in from the socket (keeps reading in)
+            sb.setLength(0); // clear buffer before appending new input
+            m = (Message)in.readObject();
+            sb.append(m.getMessage()); // adds the read in message to the StringBuffer
+            if (!usernamePhase) { // reading in email
+                if (!isValidEmail(sb.toString())) {
+                    out.writeObject(new Message(Server2.QueryType.REGISTRATION, "The email address provided is not valid! Please enter a new email address."));
+                } else {
+                    email = sb.toString(); // update email field
+                    setEmailInDatabase(email);
+                    out.writeObject(new Message(Server2.QueryType.REGISTRATION, "The email provided is of valid format! Now please enter a username that is one word in length and contains only numbers and letters."));
+                    usernamePhase = true;
+                }
+            } else if (!passwordPhase) { // in username phase
+                if (!isValidUsername(sb.toString())) {
+                    out.writeObject(new Message(Server2.QueryType.REGISTRATION, "The username provided is not valid or is already in use! Please enter a new username" +
+                            " that is only one word long and contains only numbers and letters."));
+                } else {
+                    System.out.println("didnt get here");
+                    username = sb.toString(); // update username variable
+                    setUsernameInDatabase(username);
+                    out.writeObject(new Message(Server2.QueryType.REGISTRATION, "Congratulations! Your username is now: " + sb.toString() + ". Now please enter a password!")); // print out that line to the client
+                    passwordPhase = true; // set passwordPhase to true since we will be entering it next
+                }
+            } else { // in password phase
+                if (passwordInputCount == 1) { // already entered password before so this time we want to verify it
+                    if (confirmHashOfPassword(sb.toString(), hashedPassword)) {
+                        setPasswordInDatabase(hashedPassword);
+                        out.writeObject(new Message(Server2.QueryType.REGISTRATION, "You have successfully verified your password, your account has now been created!"));
+                        break;
+                    } else {
+                        out.writeObject(new Message(Server2.QueryType.REGISTRATION, "The password you have entered does not match the one previously entered! Please input " +
+                                "a new password and then make sure that you input the same password to verify."));
+                        passwordInputCount = 0;
+                    }
+                }
+                hashedPassword = computeHashOfPassword(sb.toString()); // update password field
+                passwordInputCount++; // update the password counter to 1 so we can then verify it
+                out.writeObject(new Message(Server2.QueryType.REGISTRATION, "Now please verify your password!"));
+            }
+        }
+    }
+
+    public boolean isValidEmail(String email) {
+        if (email.equals("")) {
+            return false;
+        }
+        else {
+            // regex taken from website http://emailregex.com/
+            String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+            Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(email);
+            return matcher.matches();
+        }
+    }
+
+    public boolean isValidUsername(String username) {
+        // check if not existing in database already and that it is only one word and that it is not blank
+        return username.matches("^[a-zA-Z0-9]+");
+    }
+
+    public void setEmailInDatabase(String email) {
+        // set username in database
+    }
+
+    public void setUsernameInDatabase(String username) {
+        // set username in database
+    }
+
+    public void setPasswordInDatabase(String password) {
+        // set hashed password in database
+    }
+
+    public String computeHashOfPassword(String password) {
+        return password;
+    }
+
+    public boolean confirmHashOfPassword(String passwordToVerify, String hashOfPassword) {
+        // check if the second password input matches the first given one
+        //return passwordToVerify.equals(hashOfPassword);
+        return true;
+    }
+}
---
+++ src/registration2/Server2.java
@@ -0,0 +1,61 @@
+package registration2;
+
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server2 {
+    private static int port = 1254;
+    private static Set<String> chatNames = new HashSet<>(); // set of all client names so we can check for dupes upon registration
+    private static Map<String, Set<ObjectOutputStream>> writers = new HashMap<>(); // hash map of all printers with the key being the type of client the writer is for
+    static enum QueryType { REGISTRATION; }
+
+    public Server2(int port) {
+        this.port = port;
+    }
+
+    static Set<String> getChatNames() {
+        return chatNames;
+    }
+
+    static Map<String, Set<ObjectOutputStream>> getAllWriters() {
+        return writers; // return the hashMap of writers
+    }
+
+    static Set<ObjectOutputStream> getTypeOfWriters(String type) {
+        return writers.get(type); // return the set of writers for a given type e.g. type "REGISTRATION"
+    }
+
+    static void addWriter(String type, ObjectOutputStream os) {
+        if(getTypeOfWriters(type) == null) { // checks to see if a set of a certain type of writers already exists
+            writers.put(type, new HashSet<ObjectOutputStream>()); // if it doesnt create a new set in the hashMap
+            getTypeOfWriters(type).add(os); // add the provided writer to it
+        }
+        else {
+            getTypeOfWriters(type).add(os); // if set exists then add the provided writer to it
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("The server is running..."); // running message
+
+        ExecutorService pool = Executors.newFixedThreadPool(500); // thread pool for registration
+
+        try(ServerSocket listener = new ServerSocket(port)) {
+            while(true) {
+                pool.execute(new Handler2(listener.accept())); // execute the runnable object
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        // shutdown thread pool once we are done
+        pool.shutdown();
+    }
+}
