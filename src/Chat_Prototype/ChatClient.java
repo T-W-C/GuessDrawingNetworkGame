0bab49cf7a5e4b014bf655b02d6a5d61d260a154
Bryn Lansdown <brynlansdown@hotmail.co.uk>
2019-03-12 13:22:58 +0000

    Standalone chat prototype; will use when developing chat functionality for main game.

---
+++ src/Chat_Prototype/ChatClient.java
@@ -0,0 +1,57 @@
+package sample;
+
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+
+public class ChatClient {
+    String username;
+    String message;
+    private DataOutputStream dataos;
+    private DataInputStream datais;
+
+    ChatClient(String username, DataOutputStream dataos, DataInputStream datais) {
+        this.username = username;
+        this.dataos = dataos;
+        this.datais = datais;
+
+        new Thread(() -> { //
+            try {
+                /**
+                 * This while loop loops continuously, waiting for user input, and once the user has input a message,
+                 * the content is stored in the message variable. It gets the list of chat clients from the server,
+                 * and then sends the message to each of these clients, and writes it to the output stream of each client.
+                 */
+                while(true) {
+                    message = datais.readUTF();
+                    ChatServer.ChatClients.forEach((client) -> {
+                        DataOutputStream dataout = client.getDataos();
+                        try {
+                            dataout.writeUTF(message);
+                        } catch (Exception exception1) {
+                            exception1.printStackTrace();
+                        }
+                    });
+                }
+
+                /**
+                 * Catch statement for closing the input and output streams.
+                 */
+            } catch (Exception exception) {
+                try {
+                    dataos.close();
+                    datais.close();
+                } catch(Exception exception2) {
+                    exception2.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * Getter for dataos.
+     */
+    public DataOutputStream getDataos() {
+        return dataos;
+    }
+
+}
---
+++ src/Chat_Prototype/ClientMain.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+
+/**
+ *  ClientMain class for creating a new user instance.
+ */
+public class ClientMain extends Application {
+
+    /**
+     *  Main method.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     *  Overriding start method that loads up the login screen.
+     */
+    @Override
+    public void start(Stage loginStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        loginStage.setScene(new Scene(root, 350, 180));
+        loginStage.setTitle("Log in to the game");
+        loginStage.show();
+    }
+}
