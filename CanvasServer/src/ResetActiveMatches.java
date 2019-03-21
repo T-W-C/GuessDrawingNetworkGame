f45f5d514ec57d24e828c0c80473aa197d1ebd64
Le ファンシー Zak <ZakAmir@outlook.com>
2019-03-21 17:59:30 +0000

    Updated ResetActiveMatches class to update ports and make them in-active as-well.

--- CanvasServer/src/ResetActiveMatches.java
+++ CanvasServer/src/ResetActiveMatches.java
@@ -16,5 +16,15 @@ public class ResetActiveMatches {
         } catch (SQLException e) {
             System.out.println(e.toString());
         }
+        try (Connection activeConnection = DatabaseHandler.getInstance()
+                .getConnection()) {
+            PreparedStatement preparedStatement = null;
+            preparedStatement = activeConnection.prepareStatement("UPDATE port SET isActive = ? WHERE isActive = 'true'");
+            preparedStatement.setBoolean(1, false);
+            preparedStatement.executeUpdate();
+            System.out.println("All ports have been reset to in-active :)");
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
     }
 }
