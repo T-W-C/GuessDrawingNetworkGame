68674cf90d9b383f102a739eb91b0d2e26c42897
Le ファンシー Zak <ZakAmir@outlook.com>
2019-03-21 11:54:11 +0000

    Made a quick class to reset all active matches to inactive in database, for testing purposes.

---
+++ CanvasServer/src/ResetActiveMatches.java
@@ -0,0 +1,20 @@
+import database.DatabaseHandler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ResetActiveMatches {
+    public static void main(String[] args){
+        try (Connection activeConnection = DatabaseHandler.getInstance()
+                .getConnection()) {
+            PreparedStatement preparedStatement = null;
+            preparedStatement = activeConnection.prepareStatement("UPDATE match SET isActive = ? WHERE isActive = 'true'");
+            preparedStatement.setBoolean(1, false);
+            preparedStatement.executeUpdate();
+            System.out.println("All matches have been reset to in-active :)");
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
